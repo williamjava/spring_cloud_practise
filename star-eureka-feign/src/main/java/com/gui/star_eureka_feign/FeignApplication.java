ce5e5d9b067212bac75472f1362c869032b5cf4e
wuhoujian <wuhoujian@126.com>
2017-02-22 18:06:27 +0800

    SpringCloud-MicroServices之服务消费者feign

---
+++ star-eureka-feign/src/main/java/com/gui/star_eureka_feign/FeignApplication.java
@@ -0,0 +1,23 @@
+package com.gui.star_eureka_feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+/**
+ * Feign是一个声明式的Web Service客户端，它使得编写Web Serivce客户端变得更加简单
+ * 
+ * EnableFeignClients 注解开启Feign功能
+ * 
+ * @author wuhoujian
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class FeignApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FeignApplication.class, args);
+	}
+}
---
+++ star-eureka-feign/src/main/java/com/gui/star_eureka_feign/face/ComputeClient.java
@@ -0,0 +1,18 @@
+package com.gui.star_eureka_feign.face;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 通过注解@FeignClient来绑定该接口对应compute-service服务
+ * 
+ * @author wuhoujian
+ *
+ */
+@FeignClient("compute-service")
+public interface ComputeClient {
+	@RequestMapping(method = RequestMethod.GET, value = "/add")
+	Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
+}
