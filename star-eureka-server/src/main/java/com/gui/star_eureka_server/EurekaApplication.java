2851329523d7dc5083ef705f17637ea6d1b1d8ce
wuhoujian <wuhoujian@126.com>
2018-04-13 13:38:08 +0800

    Eureka治理中心增加高可用

--- star-eureka-server/src/main/java/com/gui/star_eureka_server/EurekaApplication.java
+++ star-eureka-server/src/main/java/com/gui/star_eureka_server/EurekaApplication.java
@@ -7,6 +7,13 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
  * 注解@EnableEurekaServer的作用：启动一个服务注册中心，其他应用通过它进行对话
  * 
+ * 如何实现高可用注册中心：
+ * 
+ * 1.两个配置文件application-peer1.yml和application-peer2.yml；
+ * 
+ * 2.java -jar xxx.jar --spring.profiles.active=peer1和java -jar xxx.jar
+ * --spring.profiles.active=peer2
+ * 
  * @author wuhoujian
  *
  */
@@ -14,7 +21,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class);
-		// SpringApplication.run(EurekaApplication.class,"--spring.profiles.active=peer1");
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 }
