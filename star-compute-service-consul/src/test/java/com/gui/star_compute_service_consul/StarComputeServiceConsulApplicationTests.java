e4b76f26054657ad80b5de6e743d6f7c2f281898
wuhoujian <wuhoujian@126.com>
2019-04-17 18:47:22 +0800

    增加consul发现与注册服务

---
+++ star-compute-service-consul/src/test/java/com/gui/star_compute_service_consul/StarComputeServiceConsulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gui.star_compute_service_consul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StarComputeServiceConsulApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
