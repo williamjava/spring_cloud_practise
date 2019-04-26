c93415064de7e036cf8ee4c0ed987c33d222a2c6
wuhoujian <wuhoujian@126.com>
2019-04-26 16:22:06 +0800

    端口统一调整

--- star-compute-service/src/main/java/com/gui/star_compute_service/ComputeServiceApplication.java
+++ star-compute-service/src/main/java/com/gui/star_compute_service/ComputeServiceApplication.java
@@ -3,7 +3,6 @@ package com.gui.star_compute_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 注解@EnableDiscoveryClient的作用： 激活Eureka中的DiscoveryClient实现，用来发现服务注册中心的其他服务
@@ -14,7 +13,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  *
  */
 @EnableCaching
-@EnableDiscoveryClient
 @SpringBootApplication
 public class ComputeServiceApplication {
 	public static void main(String[] args) {
